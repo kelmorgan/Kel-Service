a0ba9210577213b62d9158b81f69b0f3abdc1b4a
Kel Morgan <kelmorgan18@gmail.com>
2021-12-09 21:22:56 +0100

    latest service code from home intellij

--- FBNIBPSAPI/src/com/kelmorgan/initializer/ServiceInjector.java
+++ FBNIBPSAPI/src/com/kelmorgan/initializer/ServiceInjector.java
@@ -1,9 +1,7 @@
 package com.kelmorgan.initializer;
 
-import com.kelmorgan.service.Service;
-
 public interface ServiceInjector {
 
-    Service getService(String configPath);
-    Service getService(String configPath,String username, String password);
+    ServiceHandler getService(String configPath);
+    ServiceHandler getService(String configPath,String username, String password);
 }
