7b586609d022c55e673727078f50db943c6f8c9b
Kel Morgan <kelmorgan18@gmail.com>
2022-03-02 12:23:15 +0100

    latest service code from home intellij

--- FBNIBPSAPI/src/com/kelmorgan/ibpservices/controller/Controller.java
+++ FBNIBPSAPI/src/com/kelmorgan/ibpservices/controller/Controller.java
@@ -28,199 +28,210 @@ public class Controller implements Constants {
     }
 
 
-    public String getDocumentListAttached(String sessionId, String wiName,String numberOfRecords){
+    public String getDocumentListAttached(String sessionId, String wiName, String numberOfRecords) {
         logger.info("------------Get Document list api call------------------");
-        try{
-                inputXml = RequestXml.getDocumentListXml(cabinetName, sessionId, getFolderIndex(wiName), numberOfRecords);
-                logger.info("Input: " + inputXml);
-                outputXml = api.executeCall(inputXml);
-                logger.info("Output: " + outputXml);
-                return outputXml;
+        try {
+            inputXml = RequestXml.getDocumentListXml(cabinetName, sessionId, getFolderIndex(wiName), numberOfRecords);
+            logger.info("Input: " + inputXml);
+            outputXml = api.executeCall(inputXml);
+            logger.info("Output: " + outputXml);
+            return outputXml;
+        } catch (Exception e) {
+            logger.error("Exception occurred in get document list call: " + e.getMessage());
         }
-        catch (Exception e){
-            logger.error("Exception occurred in get document list call: "+ e.getMessage());
-        }
-        return  null;
+        return null;
     }
 
-    public void  addDocument(String sessionId,String folderIndex,String documentSize, String pageCount,String uploadType, String ISIndex,String appName,String docType){
+    public void addDocument(String sessionId, String folderIndex, String documentSize, String pageCount, String uploadType, String ISIndex, String appName, String docType) {
         logger.info("----------Add document in Image Server Api-------------------");
         try {
-            inputXml = RequestXml.getAddDocumentXml(cabinetName,sessionId,folderIndex,documentSize,pageCount,uploadType,ISIndex,appName,docType);
-            logger.info("inputXml: "+inputXml);
+            inputXml = RequestXml.getAddDocumentXml(cabinetName, sessionId, folderIndex, documentSize, pageCount, uploadType, ISIndex, appName, docType);
+            logger.info("inputXml: " + inputXml);
 
             outputXml = api.executeCall(inputXml);
-            logger.info("output: "+outputXml);
-        }
-        catch (Exception e){
+            logger.info("output: " + outputXml);
+        } catch (Exception e) {
             logger.error("Exception occurred");
         }
     }
 
-    public String getSessionId(String userName,String passWord){
+    public String getSessionId(String userName, String passWord) {
         logger.info("--------------Welcome to getSession method-------------------");
-        String connectXml = RequestXml.getConnectCabinetXml(cabinetName,userName,passWord);
-        logger.info("connectXml: "+connectXml);
+        String connectXml = RequestXml.getConnectCabinetXml(cabinetName, userName, passWord);
+        logger.info("connectXml: " + connectXml);
         try {
             String connectOutputXml = api.executeCall(connectXml);
-            logger.info("connectOutputXml: "+connectOutputXml);
-            System.out.println("output from connect api:"+connectOutputXml);
+            logger.info("connectOutputXml: " + connectOutputXml);
+            System.out.println("output from connect api:" + connectOutputXml);
             xmlParser.setInputXML(connectOutputXml);
 
             if (success(xmlParser.getValueOf("MainCode")))
                 return xmlParser.getValueOf("SessionId");
 
         } catch (Exception e) {
-           logger.error("Exception occurred in getSessionId Method: "+e.getMessage());
+            logger.error("Exception occurred in getSessionId Method: " + e.getMessage());
         }
         return null;
     }
-    public String getSessionId(){
+
+    public String getSessionId() {
         logger.info("---------------Welcome to getSession method-------------------");
-        String connectXml = RequestXml.getConnectCabinetXml(cabinetName,username,password);
-        logger.info("connectXml: "+connectXml);
+        String connectXml = RequestXml.getConnectCabinetXml(cabinetName, username, password);
+        logger.info("connectXml: " + connectXml);
         try {
             String connectOutputXml = api.executeCall(connectXml);
-            logger.info("connectOutputXml: "+connectOutputXml);
-            System.out.println("output from connect api:"+connectOutputXml);
+            logger.info("connectOutputXml: " + connectOutputXml);
+            System.out.println("output from connect api:" + connectOutputXml);
             xmlParser.setInputXML(connectOutputXml);
 
             if (success(xmlParser.getValueOf("MainCode")))
                 return xmlParser.getValueOf("SessionId");
 
         } catch (Exception e) {
-            logger.error("Exception occurred in getSessionId Method: "+e.getMessage());
+            logger.error("Exception occurred in getSessionId Method: " + e.getMessage());
         }
         return null;
     }
-    public String getCreatedWorkItem(String sessionId,String attributes,String processDefId, String queueId,String initiateFlag){
+
+    public String getCreatedWorkItem(String sessionId, String attributes, String processDefId, String queueId, String initiateFlag) {
         logger.info("--------------Welcome to Created WorkItem method-------------------");
-        inputXml = RequestXml.getCreateWorkItemXml(cabinetName,sessionId,processDefId,queueId,attributes,initiateFlag);
-        logger.info("inputXml: "+inputXml);
-        System.out.println("input from upload api:"+inputXml);
+        String inputXml = RequestXml.getCreateWorkItemXml(cabinetName, sessionId, processDefId, queueId, attributes, initiateFlag);
+        logger.info("inputXmlNew : " + inputXml);
+        System.out.println("input from upload api:" + inputXml);
         try {
-            outputXml = api.executeCall(inputXml);
-            logger.info("outputXml: "+outputXml);
-            System.out.println("output from upload api:"+outputXml);
+            String outputXml = api.executeCall(inputXml);
+            logger.info("outputXmlNew : " + outputXml);
+            System.out.println("output: " + outputXml);
             xmlParser.setInputXML(outputXml);
-            if(success(xmlParser.getValueOf("MainCode")))
+            if (success(xmlParser.getValueOf("MainCode")))
                 return xmlParser.getValueOf("ProcessInstanceId");
         } catch (Exception e) {
-            logger.error("Exception occurred in create workitem Method: "+e.getMessage());
+            logger.error("Exception occurred in create workitem Method: " + e.getMessage());
         }
         return null;
     }
-    public String completeWorkItem (String sessionId, String wiName) {
+
+    public String completeWorkItem(String sessionId, String wiName) {
         logger.info("------------------------Welcome to Complete WorkItem method------------------------");
-        inputXml = RequestXml.getCompleteWorkItemXml(cabinetName,sessionId,wiName);
-        logger.info("inputXml: "+inputXml);
-        System.out.println("input from completeworkitem-- "+inputXml);
+        inputXml = RequestXml.getCompleteWorkItemXml(cabinetName, sessionId, wiName);
+        logger.info("inputXml: " + inputXml);
+        System.out.println("input from completeworkitem-- " + inputXml);
 
         try {
             outputXml = api.executeCall(inputXml);
-            logger.info("outputXml: "+outputXml);
-            System.out.println("outputXml from completeworkitem-- "+outputXml);
+            logger.info("outputXml: " + outputXml);
+            System.out.println("outputXml from completeworkitem-- " + outputXml);
             xmlParser.setInputXML(outputXml);
 
-            if (success(xmlParser.getValueOf("MainCode")))  return apiSuccess;
+            if (success(xmlParser.getValueOf("MainCode"))) return apiSuccess;
         } catch (Exception e) {
-            logger.error("Exception occurred in complete Method: "+e.getMessage());
+            logger.error("Exception occurred in complete Method: " + e.getMessage());
         }
         return apiFailed;
     }
-    public void lockWorkItem(String sessionId,String wiName){
+
+    public void lockWorkItem(String sessionId, String wiName) {
         logger.info("---------------------Welcome to Lock WorkItem method----------------------");
-        inputXml = RequestXml.getLockWorkItemInputXml(cabinetName,sessionId,wiName);
-        logger.info("inputXml: "+inputXml);
+        inputXml = RequestXml.getLockWorkItemInputXml(cabinetName, sessionId, wiName);
+        logger.info("inputXml: " + inputXml);
 
-        System.out.println("input from lock workitem-- "+inputXml);
+        System.out.println("input from lock workitem-- " + inputXml);
 
         try {
             outputXml = api.executeCall(inputXml);
-            logger.info("outputXml: "+outputXml);
-            System.out.println("outputXml from lock workitem-- "+outputXml);
+            logger.info("outputXml: " + outputXml);
+            System.out.println("outputXml from lock workitem-- " + outputXml);
         } catch (Exception e) {
-            logger.error("Exception occurred in lock workitem Method: "+e.getMessage());
+            logger.error("Exception occurred in lock workitem Method: " + e.getMessage());
         }
     }
-    public void unlockWorkItem (String sessionId,String wiName){
+
+    public void unlockWorkItem(String sessionId, String wiName) {
         logger.info("--------------------Welcome to unlock WorkItem method--------------------------");
-        inputXml = RequestXml.getUnlockWorkItemXml(cabinetName,sessionId,wiName);
-        logger.info("inputXml: "+inputXml);
-        System.out.println("input from unlock workitem-- "+inputXml);
+        inputXml = RequestXml.getUnlockWorkItemXml(cabinetName, sessionId, wiName);
+        logger.info("inputXml: " + inputXml);
+        System.out.println("input from unlock workitem-- " + inputXml);
 
         try {
             outputXml = api.executeCall(inputXml);
-            logger.info("outputXml: "+outputXml);
-            System.out.println("outputXml from unlock workitem-- "+outputXml);
+            logger.info("outputXml: " + outputXml);
+            System.out.println("outputXml from unlock workitem-- " + outputXml);
 
         } catch (Exception e) {
-            logger.error("Exception occurred in unlock workitem Method: "+e.getMessage());
+            logger.error("Exception occurred in unlock workitem Method: " + e.getMessage());
         }
     }
-    public void setAttribute(String sessionId,String wiName,String attributeName, String value){
+
+    public void setAttribute(String sessionId, String wiName, String attributeName, String value) {
         logger.info("Welcome to setAttribute method");
-        inputXml = RequestXml.getAssignAttributeXml(cabinetName,sessionId,wiName,attributeName,value);
-        logger.info("inputXml: "+inputXml);
-        System.out.println("input from setAttribute-- "+ inputXml);
+        inputXml = RequestXml.getAssignAttributeXml(cabinetName, sessionId, wiName, attributeName, value);
+        logger.info("inputXml: " + inputXml);
+        System.out.println("input from setAttribute-- " + inputXml);
 
         try {
             outputXml = api.executeCall(inputXml);
-            System.out.println("outputXml from setAttribute-- "+ outputXml);
+            System.out.println("outputXml from setAttribute-- " + outputXml);
         } catch (Exception e) {
-            logger.error("Exception occurred in setAttribute Method: "+e.getMessage());
+            logger.error("Exception occurred in setAttribute Method: " + e.getMessage());
         }
     }
-    public void sendMail(String sessionId, String activityId, String processInstanceId,String processDefId, String mailFrom, String mailTo, String mailCC, String mailSubject, String mailMessage   ) {
+
+    public void sendMail(String sessionId, String activityId, String processInstanceId, String processDefId, String mailFrom, String mailTo, String mailCC, String mailSubject, String mailMessage) {
         logger.info("-----------------Welcome to sendMail method----------------------");
         inputXml = RequestXml.getAddToMailQueue(cabinetName, sessionId, processDefId, activityId, processInstanceId, mailFrom, mailTo, mailCC, mailSubject, mailMessage);
-        logger.info("inputXml: "+inputXml);
+        logger.info("inputXml: " + inputXml);
         try {
             outputXml = api.executeCall(inputXml);
-            logger.info("outputXml: "+outputXml);
+            logger.info("outputXml: " + outputXml);
 
         } catch (Exception e) {
-            logger.error("Exception occurred in sendMail Method: "+e.getMessage());
+            logger.error("Exception occurred in sendMail Method: " + e.getMessage());
         }
 
     }
-    public void disconnectSession (String sessionId){
+
+    public void disconnectSession(String sessionId) {
         logger.info("-------------------Welcome to Disconnect Session method---------------------");
-        inputXml = RequestXml.getDisconnectCabinetXml(cabinetName,sessionId);
-        logger.info("inputXml: "+inputXml);
-        System.out.println("input from disconnect cabinet-- "+inputXml);
+        inputXml = RequestXml.getDisconnectCabinetXml(cabinetName, sessionId);
+        logger.info("inputXml: " + inputXml);
+        System.out.println("input from disconnect cabinet-- " + inputXml);
         try {
             outputXml = api.executeCall(inputXml);
-            logger.info("outputXml: "+outputXml);
-            System.out.println("outputXml from disconnect cabinet-- "+outputXml);
+            logger.info("outputXml: " + outputXml);
+            System.out.println("outputXml from disconnect cabinet-- " + outputXml);
         } catch (Exception e) {
-            logger.error("Exception occurred in disconnect cabinet Method: "+e.getMessage());
+            logger.error("Exception occurred in disconnect cabinet Method: " + e.getMessage());
         }
     }
-    public Set<Map<String,String>> getRecords(String query){
+
+    public Set<Map<String, String>> getRecords(String query) {
         logger.info("-----------------Welcome to getRecords method--------------------");
-        return new DbConnect(logger,api, RequestXml.getSelectQueryXml(query,cabinetName)).getData();
+        return new DbConnect(logger, api, RequestXml.getSelectQueryXml(query, cabinetName)).getData();
     }
-    public int updateRecords(String sessionId, String tableName, String columnName, String values, String condition){
+
+    public int updateRecords(String sessionId, String tableName, String columnName, String values, String condition) {
         logger.info("---------------Welcome to Update Records method-------------------");
-       return new DbConnect(logger,api, RequestXml.getUpdateQueryXml(cabinetName, sessionId, tableName, columnName, values, condition)).saveData();
+        return new DbConnect(logger, api, RequestXml.getUpdateQueryXml(cabinetName, sessionId, tableName, columnName, values, condition)).saveData();
     }
-    public int insertRecords(String sessionId,String tableName,String columnName, String values){
+
+    public int insertRecords(String sessionId, String tableName, String columnName, String values) {
         logger.info("---------------Welcome to Insert Records method--------------------");
-        return new DbConnect(logger,api, RequestXml.getInsertQueryXml(cabinetName,sessionId,tableName,columnName,values)).saveData();
+        return new DbConnect(logger, api, RequestXml.getInsertQueryXml(cabinetName, sessionId, tableName, columnName, values)).saveData();
     }
-    public int deleteRecords(String sessionId,String tableName,String condition){
-        return new DbConnect(logger,api, RequestXml.getDeleteQueryXml(cabinetName,sessionId,tableName,condition)).saveData();
+
+    public int deleteRecords(String sessionId, String tableName, String condition) {
+        return new DbConnect(logger, api, RequestXml.getDeleteQueryXml(cabinetName, sessionId, tableName, condition)).saveData();
     }
-    private boolean success(String response){
+
+    private boolean success(String response) {
         return response.equalsIgnoreCase("0");
     }
 
     private String getFolderIndex(String wiName) {
-        logger.info("-----fetching folderIndex for wiName: "+wiName);
+        logger.info("-----fetching folderIndex for wiName: " + wiName);
         String query = Query.getFolderIndex(wiName);
         Set<Map<String, String>> queryOutput = getRecords(query);
-        logger.info("queryOutput: "+queryOutput);
+        logger.info("queryOutput: " + queryOutput);
 
         String folderIndexColumnName = "FolderIndex";
         for (Map<String, String> result : queryOutput)
@@ -229,7 +240,7 @@ public class Controller implements Constants {
         return null;
     }
 
-    private boolean isEmpty(String value){
+    private boolean isEmpty(String value) {
         return value.isEmpty() || value == null;
     }
 }
